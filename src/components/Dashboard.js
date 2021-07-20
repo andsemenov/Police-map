b902b0c148fea86eedc3ff54c65565b8ed75242c
Andrei <andsemenov@gmail.com>
2021-07-20 21:34:31 +0100

    added styles to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,40 +31,43 @@ function Dashboard(props) {
   }, [month, year, props]);
 
   return (
-    <>
-      <select
-        name="select"
-        onClick={(e) => {
-          setMonth(e.target.value);
-        }}
-      >
-        <option value="">Choose a Month</option>
-        <option value="01">January</option>
-        <option value="02">February</option>
-        <option value="03">March</option>
-        <option value="04">April</option>
-        <option value="05">May</option>
-        <option value="06">June</option>
-        <option value="07">July</option>
-        <option value="08">August</option>
-        <option value="09">September</option>
-        <option value="10">October</option>
-        <option value="11">November</option>
-        <option value="12">December</option>
-      </select>
-      <select
-        name="select"
-        onClick={(e) => {
-          setYear(e.target.value);
-        }}
-      >
-        <option value="">Choose a Year</option>
-        <option value="2021">2021</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-      </select>
-    </>
+    <div className="header">
+      <h1>Street-level crimes for the last available period</h1>
+      <div className="selects">
+        <select
+          name="select-month"
+          onClick={(e) => {
+            setMonth(e.target.value);
+          }}
+        >
+          <option value="">Choose a Month</option>
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">June</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
+        <select
+          name="select-year"
+          onClick={(e) => {
+            setYear(e.target.value);
+          }}
+        >
+          <option value="">Choose a Year</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+        </select>
+      </div>
+    </div>
   );
 }
 
